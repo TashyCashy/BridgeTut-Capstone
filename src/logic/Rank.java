88cf8e6ad11e84ef91458a10bce8c7a3c7b03880
Natasha Mahachi <mhcnat001@myuct.ac.za>
2026-08-12 23:06:43 +0200

    Implemented Trick skeleton using PlayerPosition to follow game rules

--- src/logic/Rank.java
+++ src/logic/Rank.java
@@ -3,13 +3,17 @@ package logic;
 public enum Rank {
     TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
 
+    /**
+     * getting the card value
+     * @return ordinal of this enumeration constant (its position in its enum declaration, where the initial constant is assigned an ordinal of zero)
+     */
     public String getRankLetter() {
         switch (this) {
             case JACK: return "J";
             case QUEEN: return "Q";
             case KING: return "K";
             case ACE: return "A";
-            default: return String.valueOf(ordinal()+2); // returns ordinal of this enumeration constant (its position in its enum declaration, where the initial constant is assigned an ordinal of zero)
+            default: return String.valueOf(ordinal()+2); 
         }
     }
 }
--- src/logic/Suit.java
+++ src/logic/Suit.java
@@ -3,6 +3,10 @@ package logic;
 public enum Suit {
     CLUBS, DIAMONDS, HEARTS, SPADES;
 
+    /**
+     * helps to determine "strength" of card for trick determination
+     * @return suit associated with card
+     */
     public String getSuitLetter() {
         switch (this) {
             case CLUBS: return "C";
