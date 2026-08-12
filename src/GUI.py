83946ae145fba200307498a8cd7add38e954f7fe
Joye <FRNJOY002@myuct.ac.za>
2026-08-12 15:06:36 +0200

    Added frames which will display the different app pages

--- src/GUI.py
+++ src/GUI.py
@@ -1,9 +1,48 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import os 
 
-root=Tk()
-root.title()
-root.geometry("900x500")
-root.configure(background="green")
+class GUI(Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title("Bridge: NiteMeh Edition")
+        self.geometry("900x500")
+
+        container= Frame(self) #Creating frames so different app pages can be displayed
+        container.pack(fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames={}
+        for F in (LoginPage, HomePage, ResultPage):
+            frame= F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(LoginPage)
+
+    def show_frame(self, page_class):
+        """Allows different pages to be displayed"""
+        frame= self.frames[page_class]
+        frame.tkraise()
+
+class LoginPage(Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, background="pink")
+        Label(self, text="").pack(pady=20)
+        Button(self, text="Sign in", command=lambda: controller.show_frame(HomePage)).pack()
+
+class HomePage(Frame):
+    def __init__(self, parent, controller):
+            super().__init__(parent)
+            Label(self, text="").pack()
+
+class ResultPage(Frame):
+    def __init__(self, parent, controller):
+            super().__init__(parent)
+            Label(self, text="").pack()
+
+
     
-root.mainloop()
+
+GUI().mainloop()
