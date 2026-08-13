30895128fa84fdc7ac961c302d0fa66c39e2920f
Natasha Mahachi <mhcnat001@myuct.ac.za>
2026-08-13 20:44:34 +0200

    added Playerhand logic: sorting the hand and suit search

--- src/logic/PlayerHand.java
+++ src/logic/PlayerHand.java
@@ -1,1 +1,67 @@
-// TreeSet to be used for sorting the player's hand
+package logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerHand {
+    private final PlayerPosition position;
+    private final List<Card> hand = new ArrayList<>();
+
+    public PlayerHand(PlayerPosition position) {
+        this.position = position;
+    }
+
+    public PlayerPosition getPlayerPosition() {
+        return this.position;
+    }
+
+    public List<Card> getHand() {
+        return this.hand;
+    }
+
+    public void addCard(Card card) {
+        hand.add(card);
+        sortHand();
+    }
+
+    public void removeCard(Card card) {
+        hand.remove(card);
+    }
+
+    public int size() {
+        return hand.size();
+    }
+
+    /**
+     * 
+     * @param suit
+     * @return true if player has cards of the given suit
+     */
+    public boolean hasSuit(Suit suit) {
+        for (Card card : hand) {
+            if (card.getSuit() == suit) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isBigger(Card one, Card two) {
+        if (one.getSuit() != two.getSuit()) {
+            return one.getSuit().ordinal() > two.getSuit().ordinal();
+        }
+        return one.getRank().ordinal() > two.getRank().ordinal();
+    }
+
+    private void sortHand() {
+        for (int i = 1; i<hand.size(); i++) {
+            Card card = hand.get(i);
+            int j = i-1;
+            while (j >= 0 && isBigger(hand.get(j), card)) {
+                hand.set(j+1, hand.get(j));
+                j--;
+            }
+            hand.set(j+1, card);
+        }
+    }
+}
---
+++ src/logic/trick.java
@@ -0,0 +1,57 @@
+package logic;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Trick {   
+    private Suit ledSuit;
+    private final Map<PlayerPosition, Card> playedCards = new LinkedHashMap<>();
+    private final PlayerPosition lead;
+
+    public Trick(PlayerPosition lead) {
+        this.lead = lead;
+    }
+
+    public PlayerPosition getLead() {
+        return this.lead;
+    }
+
+    public Suit getLedSuit() {
+        return this.ledSuit;
+    }
+
+    public Map<PlayerPosition, Card> getPlayedCards() {
+        return this.playedCards;
+    }
+
+    /**
+     * records card as played by the player
+     * doesn't check whether move is allowed or not
+     * @param player
+     * @param card
+     */
+    public void recordPlay(PlayerPosition player, Card card) {
+        if (playedCards.isEmpty())
+            this.ledSuit = card.getSuit();
+        playedCards.put(player, card);
+    }
+
+    /**
+     * decides who plays first card during partiicular trick.
+     * bridge rule: player on the declarer's left goes first
+     * @param declarer, Konke's bidding/declarer stuff
+     * @return a new. empty Trick led by the correct player
+     */
+    public static Trick startTrick(PlayerPosition declarer) {
+        PlayerPosition leader = declarer.next();
+        return new Trick(leader);
+    }
+
+    /**
+     * 
+     * @return true when all players have played a card during this trick
+     */
+    public boolean done() {
+        return playedCards.size() == 4;
+    }
+}
