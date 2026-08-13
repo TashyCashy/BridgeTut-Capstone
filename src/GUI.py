9e606b2b006b587334e7e7ab09cb6baba22004b0
Joye <FRNJOY002@myuct.ac.za>
2026-08-13 17:50:30 +0200

    Created layout for the home page, different colours displays where cards will be displayed

--- src/GUI.py
+++ src/GUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 from PIL import Image, ImageTk
 import os 
 
@@ -72,8 +73,53 @@ class LoginPage(Frame):
 
 class HomePage(Frame):
     def __init__(self, parent, controller):
-            super().__init__(parent)
-            Label(self, text="").pack()
+            super().__init__(parent, background="#0f4d3f")
+
+            self.grid_rowconfigure(0, weight=0)
+            self.grid_rowconfigure(1, weight=1)
+            self.grid_columnconfigure(0, weight=1)
+
+            header_frame= Frame(self, bg="darkgreen", height=80)
+            header_frame.grid_propagate(False)
+            table_frame= Frame(self, bg="darkgreen")
+
+            header_frame.grid(row=0, column=0, sticky="ew")
+            table_frame.grid(row=1, column=0, sticky="nsew")
+
+            Label(header_frame,
+                  text="North/South tricks: 0   East/West tricks:0",
+                  bg="darkgreen",
+                  fg="white"
+                ).grid(row=0, column=0, pady=20)
+
+            table_frame.grid_rowconfigure(0, weight=1)
+            table_frame.grid_rowconfigure(1, weight=3)
+            table_frame.grid_rowconfigure(2, weight=1)
+
+            table_frame.grid_columnconfigure(0, weight=1)
+            table_frame.grid_columnconfigure(1, weight=3)
+            table_frame.grid_columnconfigure(2, weight=1)
+
+            north_frame= Frame(table_frame, bg="red")
+            west_frame= Frame(table_frame, bg="blue")
+            centre_frame= Frame(table_frame, bg="darkgreen")
+            east_frame= Frame(table_frame, bg="purple")
+            south_frame= Frame(table_frame, bg="orange")
+
+            north_frame.grid(row=0, column=1, sticky="nsew")
+            west_frame.grid(row=1, column=0, sticky="nsew")
+            centre_frame.grid(row=1, column=1, sticky="nsew")
+            east_frame.grid(row=1, column=2, sticky="nsew")
+            south_frame.grid(row=2, column=1, sticky="nsew")
+         
+
+    def resize_cards(card):
+      card_image=Image.open(card)
+      resized_card= card_image.resize((100,100))   
+      global card_img 
+      card_img= ImageTk.PhotoImage(resized_card)
+
+      return card_img         
 
 class ResultPage(Frame):
     def __init__(self, parent, controller):
