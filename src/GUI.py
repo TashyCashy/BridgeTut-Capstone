adde6ca960db21edb6ac9bb6835b09827336ac3f
Joye <FRNJOY002@myuct.ac.za>
2026-08-13 16:48:16 +0200

    Added more functionality to the log in page

--- src/GUI.py
+++ src/GUI.py
@@ -28,9 +28,47 @@ class GUI(Tk):
 
 class LoginPage(Frame):
     def __init__(self, parent, controller):
-        super().__init__(parent, background="pink")
-        Label(self, text="").pack(pady=20)
-        Button(self, text="Sign in", command=lambda: controller.show_frame(HomePage)).pack()
+        super().__init__(parent, background="#0f4d3f")
+        self.grid_columnconfigure(0, weight=1)
+
+        welcome_label= Label(self, 
+              text="Welcome!", 
+              font=("Georgia", 36, "bold"), 
+              fg="#d4af37",
+              bg="#0f4d3f",
+              pady=20)
+        
+        username_label=Label(self, 
+                             text="Username: ",
+                             font=("Segoe UI", 12))
+        
+        username_entry=Entry(self, 
+                             width=30,
+                             font=("Segoe UI", 12))
+        
+        password_entry=Entry(self,
+                             show="*", 
+                             width=30,
+                             font=("Segoe UI", 12))
+
+        password_label=Label(self, 
+                             text="Password: ",
+                             font=("Segoe UI", 12))
+        
+        login_btn= Button(self, 
+                         text="Sign in",
+                         font=("Segoe UI", 12),
+                         padx=10,
+                         pady=5,
+                         command=lambda: controller.show_frame(HomePage))
+
+        welcome_label.grid(row=0, column=0, sticky="ew", pady=(20, 40))
+        username_label.grid(row=1, column=0, pady=(10, 5))
+        username_entry.grid(row=2, column=0)
+        password_label.grid(row=3, column=0, pady=(20, 5) )
+        password_entry.grid(row=4, column=0)
+        login_btn.grid(row=5, column=0, pady=30)
+
 
 class HomePage(Frame):
     def __init__(self, parent, controller):
