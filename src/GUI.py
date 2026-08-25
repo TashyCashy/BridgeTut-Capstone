e204904a63d569f4f64f439e9e91a02a8dc42127
Joye <FRNJOY002@myuct.ac.za>
2026-08-25 16:52:54 +0200

    Added double button, cleaned up code

--- src/GUI.py
+++ src/GUI.py
@@ -4,15 +4,13 @@ from PIL import Image, ImageTk
 import os 
 
 class GUI(Tk):
-
     def __init__(self):
         super().__init__()
         self.title("Bridge: NiteMeh Edition")
         self.geometry("1400x900")
-        self.minsize(1100, 750)
-        self.configure(bg="#10251D")
+        self.minsize(1100, 750) #fixed minimum size which accomodates the playing table.
 
-        #Image storage
+        #Image storage and fixed image measurements
         self.card_images={}
         self.card_width=70
         self.card_height=100
@@ -38,19 +36,19 @@ class GUI(Tk):
 
 class LoginPage(Frame):
     def __init__(self, parent, controller):
-        super().__init__(parent, background="#0f4d3f")
+        super().__init__(parent, background="#055341")
         self.controller= controller
 
         Label(self,
               text="♣♦♥♠ BRIDGE: NiteMeh Edition ♣♦♥♠",
               font=("Georgia", 32, "bold"),
-              bg="#0f4d3f",
-              fg="#D4AF37").pack(pady=(80,5))
+              bg="#055341",
+              fg="#C9A42C").pack(pady=(80,5))
 
         Label(self,
               text="Becoming a pro, one game at a time",
               font=("Georgia", 26, "italic"),
-              fg="#D4AF37").pack(pady=(0,35))
+              fg="#C9A42C").pack(pady=(0,35))
 
         #Creating the login panel
         login= Frame(self, background="#123f35")
@@ -73,8 +71,8 @@ class LoginPage(Frame):
         Button(login,
                text="Sign in",
                font=("Arial",12,"bold"),
-               bg="#D4AF37",
-               fg="#123f35",
+               bg="#C9A42C",
+               fg="#055341",
                relief="flat",
                command=lambda: controller.show_frame(HomePage)).pack(fill="x", padx=60, ipady=10)
 
@@ -84,29 +82,30 @@ class HomePage(Frame):
           super().__init__(parent, background="#0f4d3f")
           self.controller= controller
 
-          menu= Frame(self, bg="#0f4d3f")
+          #creating a menu frame, where user can choose mode
+          menu= Frame(self, bg="#055341")
           menu.place(relx=0.5, rely=0.5, anchor="center")
 
           Label(menu,
                 text="Welcome!",
                 font=("Georgia", 38, "italic"),
-                bg="#0f4d3f",
+                bg="#055341",
                 fg="white").pack(pady=(0,40))
 
           Button(menu,
                  text="Play Game",
                  font=("Arial",13,"bold"),
-                 bg="#126B4F",
+                 bg="#055341",
                  width=40,
                  fg="white",
-                 relief="flat",
+                 relief="flat", #Creates no border effect on the frame
                  command=lambda: controller.show_frame(GamePage)).pack(fill="x",pady=18, ipady=10)
 
           Button(menu,
                  text="Tutorial",
                  font=("Arial",13,"bold"),
                  width=40,
-                 bg="#126B4F",
+                 bg="#055341",
                  fg="white",
                  relief="flat",
                  command=lambda: controller.show_frame(TutorialPage)).pack(fill="x",pady=18, ipady=10)
@@ -114,7 +113,7 @@ class HomePage(Frame):
           Button(menu,
                  text="Game History",
                  font=("Arial",13,"bold"),
-                 bg="#126B4F",
+                 bg="#055341",
                  width=40,
                  fg="white",
                  relief="flat",
@@ -128,10 +127,8 @@ class GamePage(Frame):
 
              #Initialising game states
              self.card_images=[]
-             self.bid_pos=0
              self.current_player=0
              self.selected_level=None
-             self.selected_suit=None
              self.players=["West","North","East","South"]
              self.bid_history_data=[]
              self.bidding_phase=True
@@ -147,11 +144,12 @@ class GamePage(Frame):
              self.player_table()
              self.bidding_panel()
              self.player_hands()
+             
      def header_display(self):
              #Creating header which has the option to go back to menu and clues
-             header= Frame(self, bg="#123f35", height=60)
+             header= Frame(self, bg="#055341", height=60)
              header.grid(row=0, column=0, sticky="ew")
-             header.grid_propagate(False)
+             header.grid_propagate(False) #Ensuring fixed size of the header
              
              Label(header,
                    text="North/South tricks: 0   East/West tricks:0",
@@ -162,7 +160,7 @@ class GamePage(Frame):
              Button(header,
                     text="View Bids",
                      font=("Arial",13,"bold"),
-                     bg="#126B4F",
+                     bg="#055341",
                      fg="white",
                      relief="flat",
                      command= self.show_bids).pack(side="right", padx=10)
@@ -170,7 +168,7 @@ class GamePage(Frame):
              menu_button= Menubutton(header,
                                      text="Menu",
                                      font=("Arial",13,"bold"),
-                                     bg="#126B4F",
+                                     bg="#055341",
                                      fg="white",
                                      relief="flat")
              menu_button.pack(side="right", padx=30)
@@ -178,7 +176,7 @@ class GamePage(Frame):
              drop_down= Menu(menu_button,
                              tearoff=0,
                              bg="white",
-                             fg="#126B4F",
+                             fg="#055341",
                              font=("Arial",11))
              
              drop_down.add_command(label="Home",
@@ -195,16 +193,16 @@ class GamePage(Frame):
              
              menu_button.config(menu=drop_down)
 
-             #Creating game table
      def player_table(self):
+             """Creates player table where games take place"""
              self.table= Frame(self,
-                          bg="#126B4F",
-                          bd=5,
+                          bg="#055341",
+                          bd=5,  #setting border width of the table frame
                           relief="ridge")
              self.table.grid(row=1, column=0, sticky="nsew")
              self.table.grid_propagate(False)
              
-             #Creating correct table grid
+             #Creating desired table grid
              self.table.grid_rowconfigure(0, minsize=90)
              self.table.grid_rowconfigure(1, minsize=520)
              self.table.grid_rowconfigure(2, minsize=90)
@@ -214,12 +212,13 @@ class GamePage(Frame):
              self.table.grid_columnconfigure(2, minsize=80, weight=0)
 
              #creating 4 frames for the 4 hand display and the centre where cards get played
-             self.north_frame= Frame(self.table, bg="#126B4F")
-             self.west_frame= Frame(self.table, bg="#126B4F", width=120)
+             self.north_frame= Frame(self.table, bg="#055341")
+             self.west_frame= Frame(self.table, bg="#055341", width=120)
              self.centre_frame= Frame(self.table, bg="darkgreen", bd=3, relief="ridge")
-             self.east_frame= Frame(self.table, bg="#126B4F", width=120)
-             self.south_frame= Frame(self.table, bg="#126B4F")
+             self.east_frame= Frame(self.table, bg="#055341", width=120)
+             self.south_frame= Frame(self.table, bg="#055341")
 
+             #Ensuring west and east frames stay fixed for card displays
              self.west_frame.grid_propagate(False)
              self.east_frame.grid_propagate(False)
 
@@ -241,16 +240,16 @@ class GamePage(Frame):
                         "West": (-25,0)}
              self.trick_offsets= offsets
              for player in self.players:
-                   lbl=Label(self.centre_frame, bd=0)
+                   lbl=Label(self.centre_frame, bd=0, bg="darkgreen")
                    lbl.place(relx=0.5, rely=0.5, anchor="center",
                              x=offsets[player][0], y= offsets[player][1])
                    self.trick_labels[player]=lbl
              
 
-             #Creating bidding panel
      def bidding_panel(self):
+             """ Creating bidding panel where bids take place"""
              self.bidding=Frame(self.centre_frame,
-                           bg="#a9cdf0",
+                           bg="#7B7D7E",
                            bd=2,
                            relief="ridge")
              self.bidding.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
@@ -258,102 +257,119 @@ class GamePage(Frame):
              Label(self.bidding,
                    text="Bidding",
                    font=("Arial", 16, "bold"),
-                   bg="#a9cdf0",
+                   bg="#7B7D7E",
                    fg="white").pack(pady=(8,5))
              
-             self.players_frame=Frame(self.bidding, bg="#a9cdf0")
+             self.players_frame=Frame(self.bidding, bg="#7B7D7E")
              self.players_frame.pack(fill="x", padx=20)
 
+             #Ensuring that bids display as desired
              for col in range(4):
                    self.players_frame.grid_columnconfigure(col, weight=1, uniform="playercol")
-             
+
+             #Adding player names to display which player made which bid
              for col, p in enumerate(self.players):
                   Label(self.players_frame,
                         text=p,
                         font=("Arial", 11, "bold"),
-                        bg="#a9cdf0",
+                        bg="#7B7D7E",
                         fg="white").grid(row=0, column=col, sticky="w", padx=10)
 
              #storing the bidding history
-             self.bid_history= Frame(self.bidding, bg="#a9cdf0")
+             self.bid_history= Frame(self.bidding, bg="#7B7D7E")
              self.bid_history.pack(fill="both", expand=True, padx=20, pady=5)
 
              for col in range(4):
                    self.bid_history.grid_columnconfigure(col, weight=1, uniform="bidcol")
 
+             #Displays current contract of each player
              self.contract = Label(self.bidding,
                                    text="Current contract: None",
                                    font=("Arial", 14, "bold"),
-                                   bg="#a9cdf0",
-                                   fg="#123f35")
+                                   bg="#7B7D7E",
+                                   fg="#055341")
              self.contract.pack(pady=5)
+
              #calling create_bid_btns which displays the various button options
              self.create_bid_btns()
+
              #Added close button for testing purposes
              Button(self.bidding,
                     text="Close Bidding",
                     font=("Arial", 11, "bold"),
-                    bg="#126B4F",
+                    bg="#055341",
                     fg="white",
                     relief="flat",
                     command=lambda: self.bidding.grid_remove()).pack(pady=10)
 
      def create_bid_btns(self):
-             btn_frame= Frame(self.bidding, bg="#a9cdf0")
+             """Creates bidding buttons"""
+             btn_frame= Frame(self.bidding, bg="#7B7D7E")
              btn_frame.pack(fill="x", pady=5)
 
-             numbers= Frame(btn_frame, bg="#a9cdf0")
+             numbers= Frame(btn_frame, bg="#7B7D7E")
              numbers.pack(pady=3)
 
              Label(numbers,
                    text= "Level: ",
                    font=("Arial", 11, "bold"),
-                   bg="#a9cdf0",
-                   fg="#123f35",
+                   bg="#7B7D7E",
+                   fg="white",
                    width=7).pack(side="left", padx=5)
 
+             #Creating different contract level buttons
              self.level_btns=[]
              for num in range(1,8):
                    l_btn=Button(numbers,
                           text= str(num),
-                          font=("Arial", 10, "bold"),
+                          font=("Arial", 12, "bold"),
+                          width=3,
+                          padx=4,
+                          pady=4,
                           command=lambda n=num: self.select_level(n))
-                   l_btn.pack(side="left", padx=2)
+                   l_btn.pack(side="left", padx=3)
                    self.level_btns.append(l_btn)
 
-             suits= Frame(btn_frame, bg="#a9cdf0")
+             suits= Frame(btn_frame, bg="#7B7D7E")
              suits.pack(pady=3)
 
              Label(suits,
                    text="Suits: ",
                    font=("Arial", 11, "bold"),
-                   bg="#a9cdf0",
-                   fg="#123f35",
+                   bg="#7B7D7E",
+                   fg="white",
                    width=7).pack(side="left", padx=5)
 
-             suit = ["♣","♦","♥","♠","NT" ]
+             #Creating different suit and game logic buttons
+             suit = ["♣","♦","♥","♠","NT", "Dbl"]
              self.suits_btn=[]
              for s in suit:
                   s_btn=Button(suits,
                         text= s,
-                        font=("Arial", 10, "bold"),
-                        fg= "#123f35",
+                        font=("Arial", 12, "bold"),
+                        width=3,
+                        padx=4,
+                        pady=4,
+                        fg= "#055341",
                         command=lambda st=s: self.select_suit(st))
                   s_btn.pack(side="left", padx=2)
                   self.suits_btn.append(s_btn)
 
+             #Pass button for when player does not want to make a contract
              Button(btn_frame,
                   text= "Pass",
-                  font=("Arial", 10, "bold"),
+                  font=("Arial", 12, "bold"),
+                  width=5,
                   command=lambda: self.make_bid("Pass")).pack(pady=4)
              
      def select_level(self, level):
+           """Highligts the clicked level button"""
            self.selected_level=level
            for i, btn in enumerate(self.level_btns, start=1):
                  btn.config(relief="sunken" if i==level else "raised")
-           print("Selected level: ", level)
 
      def select_suit(self, suit):
+           """Ensures that a level is chosen first and highlights clicked button"""
            if self.selected_level is None:
                  print("Select a level first.")
                  return
@@ -365,6 +381,8 @@ class GamePage(Frame):
                          
                   
      def make_bid(self, bid):
+             """Adds and displays bid made by user"""
+             #Mostly backend but added for testing purposes
              player= self.players[self.current_player]
              self.bid_history_data.append((player, bid))
              self.display_bid(player, bid)
@@ -384,12 +402,14 @@ class GamePage(Frame):
                    self.finish_bidding()
 
      def update_lvl(self):
+           """Displays lower contract level buttons once clicked and a round of bidding passed"""
            min_lvl= getattr(self, "current_level", 0)
            for i, btn in enumerate(self.level_btns, start=1):
                  btn.config(state="disabled" if i < min_lvl else "normal")
 
 
      def display_bid(self, player, bid):
+           """Displays and stores bids made by players"""
            player_idx=self.players.index(player)
            bid_num= len(self.bid_history_data)-1
            row= (bid_num // 4)+1
@@ -397,24 +417,25 @@ class GamePage(Frame):
            Label(self.bid_history,
                  text=bid,
                  font=("Arial", 11, "bold"),
-                 bg="#a9cdf0",
-                 fg="#123f35").grid(row=row, column=player_idx, sticky="w", padx=10, pady=4)          
+                 bg="#7B7D7E",
+                 fg="white").grid(row=row, column=player_idx, sticky="w", padx=10, pady=4)          
 
      def show_bids(self):
+           """Displays bidding history whenever user clicks 'view bids' button"""
            bid_window= Toplevel(self)
            bid_window.title("Bidding History")
            bid_window.geometry("650x450")
-           bid_window.configure(bg="#0f4d3f")
+           bid_window.configure(bg="#055341")
 
            Label(bid_window,
                  text="Bidding History",
                  font=("Georgia", 22, "bold"),
-                 bg="#0f4d3f",
-                 fg="#D4AF37").pack(pady=15)
+                 bg="#055341",
+                 fg="#C9A42C").pack(pady=15)
 
            hist= Frame(bid_window,
                        bd=2,
-                       bg="#a9cdf0",
+                       bg="#7B7D7E",
                        relief="ridge")
            hist.pack(fill="both", expand=True, padx=30, pady=10)
 
@@ -422,8 +443,8 @@ class GamePage(Frame):
                  Label(hist,
                        text= player,
                        font=("Arial", 11, "bold"),
-                       bg="#a9cdf0",
-                       fg="#123f35",
+                       bg="#7B7D7E",
+                       fg="white",
                        width=7).grid(row=0, column=column, padx=20, pady=10)
 
            for i, (player, bid) in enumerate(self.bid_history_data):
@@ -432,25 +453,27 @@ class GamePage(Frame):
                  Label(hist,
                        text= bid,
                        font=("Arial", 11, "bold"),
-                       bg="#a9cdf0",
-                       fg="#123f35").grid(row=row, column=col, padx=20, pady=5)
+                       bg="#7B7D7E",
+                       fg="white").grid(row=row, column=col, padx=20, pady=5)
 
            Label(bid_window,
                   text= self.contract.cget("text"),
                   font=("Arial", 14, "bold"),
-                  bg="#0f4d3f",
+                  bg="#055341",
                   fg="white").pack(pady=5)
 
            Button(bid_window,
                   text="Close",
                   font=("Arial",11, "bold"),
-                  bg="#D4AF37",
+                  bg="#C9A42C",
                   fg="#123f35",
                   relief="flat",
                   command=bid_window.destroy).pack(pady=10)
                  
 
      def player_hands(self):
+           """Displays player hands"""
+           #calculation for west and east hands which ensures that all 13 cards are displayed
            frame_height = 520
            card_height = 100
            n = 13
@@ -502,6 +525,7 @@ class GamePage(Frame):
                btn.place(x=0,y=i * step)
 
      def play_card(self, image, player, btn):
+        """moves card to playing board and removes it from player hand"""
         if self.bidding_phase:
               self.finish_bidding()
         if player is None:
@@ -513,21 +537,25 @@ class GamePage(Frame):
 
         btn.destroy()
 
+        #board gets cleared once all 4 players have played
         self.trick_count= getattr(self, "trick_count",0)+1
         if self.trick_count==4:
               self.after(1200, self.clear_trick)
 
      def clear_trick(self):
+           """board gets cleared once all 4 players have played """
            for lbl in self.trick_labels.values():
                  lbl.config(image="")
                  lbl.image=None
            self.trick_count=0
 
      def finish_bidding(self):
+           """removes bidding panel once bidding has been completed"""
            self.bidding_phase=False
            self.bidding.grid_remove()
     
      def resize_cards(self, card):
+        """Ensures cards are shaped in a way that it can be displayed by player hands and on the board"""
         card_image=Image.open(card)
         resized_card= card_image.resize((70,100))   
         return ImageTk.PhotoImage(resized_card)
