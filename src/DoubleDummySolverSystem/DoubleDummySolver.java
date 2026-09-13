dc2f7b3d313bc4965bfc947bb32440a64c439e3a
Konke Vezi <konkevezi17@gmail.com>
2026-09-13 22:02:53 +0200

    Added alpha and beta pruning to imrpove speed of DDS

--- src/DoubleDummySolverSystem/DoubleDummySolver.java
+++ src/DoubleDummySolverSystem/DoubleDummySolver.java
@@ -6,7 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DoubleDummySolver {
+
     static int solve(GameState state){
+        int alpha = -1;
+        int beta = 14;
+        return solve(state, alpha, beta);
+    }
+    //overloaded method for alpha-beta pruning, used to optimise the solver method, not checking other branches if we've already found the best possible recursive sequence
+
+    static int solve (GameState state, int alpha, int beta){
         //base case
         if (state.isHandComplete()) { return 0;}
         //given a gamestate do the double dummy analysis
@@ -37,17 +45,22 @@ public class DoubleDummySolver {
                     bonus = 1;
                 }
                 else { //min side won
-                bonus = 0;
+                    bonus = 0;
                 }
             }
             else{
                 bonus = 0;// bonus stays zero if trick is not over
             }
             //value of child node, kicks off recursion
-            childValue = bonus + solve(nextState);
+            childValue = bonus + solve(nextState, alpha - bonus, beta - bonus);
             bestValue = (MAX) ? Math.max(bestValue,childValue) : Math.min(bestValue,childValue);
+
+            if (MAX) { alpha = Math.max(alpha, bestValue); }
+            else { beta = Math.min(beta, bestValue); }
+            if (alpha >= beta) break;
         }
         return bestValue;
+
     }
     //check if player is on the max side or the min side, true if max, false if min
     //Max is the team of the declarer, and min is the opposing team
