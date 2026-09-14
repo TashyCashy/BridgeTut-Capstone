fb1b49a1385d95a6e417a20151617be309a6fa4e
Natasha Mahachi <mhcnat001@myuct.ac.za>
2026-09-14 16:25:45 +0200

    Add TutorialGateway to play execution methods and expose simple status to python

---
+++ src/BiddingSystem/Tutorial/TutorialGateway.java
@@ -0,0 +1,29 @@
+package BiddingSystem.Tutorial;
+
+public class TutorialGateway {
+    private final TutorialEngine engine;
+
+    public TutorialGateway(TutorialEngine engine) {
+        this.engine = engine;
+    }
+
+    public int getCurrentTurnSeatIndex() {
+        return engine.getCurrentTurnSeatIndex();
+    }
+
+    public int getMistakeCount() {
+        return engine.getMistakeCount();
+    }
+
+    public boolean isTutorialComplete() {
+        return engine.isTutorialComplete();
+    }
+
+    public boolean playCard(int seatIdx, String cardCode) {
+        return engine.playCard(seatIdx, cardCode);
+    }
+
+    public String getLessonNote() {
+        return engine.getLessonNote();
+    }
+}
